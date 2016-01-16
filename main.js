e245011a5e8f76e72d4083aac08ccde911f40960
Daniel de la Cruz <danieldelacruz01@gmail.com>
2016-01-16 19:35:40 +1300

    add variable track length

--- main.js
+++ main.js
@@ -1,46 +1,65 @@
+$(document).ready(function(){
+  //start state
   var redMoves = 0;
   var blueMoves = 0;
 
-// keydown event
-$(document).keyup(function(key){
+  var trackLength = prompt("How long do you want the track to be?", "Set a track length between 10 - 30");
+  console.log (trackLength);
 
-  if (key.keyCode === 80) {
 
-    if (redMoves < 19){
-      updatePlayerPosition("red");
-      redMoves++;
-    } else {
-      winnerBox("Red")
+  var restart = function() {
+    $("td").removeClass("active");
+    $("#blue td:first").addClass("active");
+    $("#red td:first").addClass("active");
+
+    redMoves = 0;
+    blueMoves = 0;
+  };
+
+    for (var i = 1; i <= trackLength; i++) {
+      console.log(i);
+      $(".track").append("<td>");
     };
 
-  } else if (key.keyCode === 81) {
+  restart();
 
-    if (blueMoves < 19){
-      updatePlayerPosition("blue");
-      blueMoves++;
+  // keydown event
+  $(document).on('keyup', function(key){
 
-    } else {
-      winnerBox("Blue")
+    if (key.keyCode === 80) {
+
+      if (redMoves < trackLength - 1){
+        updatePlayerPosition("red");
+        redMoves++;
+      } else {
+        winnerBox("Red")
+      };
+
+    } else if (key.keyCode === 81) {
+
+      if (blueMoves < trackLength - 1){
+        updatePlayerPosition("blue");
+        blueMoves++;
+
+      } else {
+        winnerBox("Blue")
+      };
     };
+  });
+
+  //move active td
+  var updatePlayerPosition = function (playerColor) {
+    var activeCell = $("#" + playerColor + " td.active");
+    var moveCell = activeCell.next();
+
+    activeCell.removeClass("active");
+    moveCell.addClass("active");
+  };
+
+  //winning state
+  var winnerBox = function (winner) {
+    window.alert (winner + " wins! Press OK to play again.")
+    restart();
+
   };
 });
-
-var updatePlayerPosition = function (player) {
-  var activeCell = $("#" + player + " td.active");
-  var moveCell = activeCell.next();
-
-  activeCell.removeClass("active");
-  moveCell.addClass("active");
-};
-
-//winning state
-var winnerBox = function (winner) {
-  window.alert (winner + " wins! Press OK to play again.")
-
-  $("td").removeClass("active");
-  $("#blue td:first").addClass("active");
-  $("#red td:first").addClass("active");
-
-  redMoves = 0;
-  blueMoves = 0;
-};
