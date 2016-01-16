a5d78c59e5ff26e47626eb5d787908c4d340b8e4
Daniel de la Cruz <danieldelacruz01@gmail.com>
2016-01-16 15:38:34 +1300

    change active cell to next cell on keyup event

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
-// var updatePlayerPosition() {
-
-// };
 
 
 // keydown event
-$(document).keydown(function(key){
+$(document).keyup(function(key){
+
+  var activeCell = $("td.active");
+  var moveCell = activeCell.next();
+
   if (key.keyCode === 80) {
+
     console.log ("p");
+    
+    activeCell.removeClass("active");
+    moveCell.addClass("active");
+
   } else if (key.keyCode === 81) {
     console.log ("q");
   };
