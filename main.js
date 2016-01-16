79f2df8098c38e4a234b65cf4b10aea66c53ee3f
Daniel de la Cruz <danieldelacruz01@gmail.com>
2016-01-16 16:51:19 +1300

    add winning state

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-
+  var redMoves = 0;
+  var blueMoves = 0;
 
 // keydown event
 $(document).keyup(function(key){
@@ -6,23 +7,43 @@ $(document).keyup(function(key){
 
   if (key.keyCode === 80) {
 
-    console.log ("p");
-    updatePlayerPosition(red);
-    
+    // console.log ("p");
+    if (redMoves < 19){
+      updatePlayerPosition("red");
+      redMoves++;
+
+    } else {
+      winnerBox("Red")
+
+    };
 
   } else if (key.keyCode === 81) {
 
-    console.log ("q");
-    updatePlayerPosition(blue);
+    if (blueMoves < 19){
+      updatePlayerPosition("blue");
+      blueMoves++;
+
+    } else {
+      winnerBox("Blue")
+    };
 
 
   };
 });
 
 var updatePlayerPosition = function (player) {
-  var activeCell = $("#p" + player + " td.active");
+
+  var activeCell = $("#" + player + " td.active");
   var moveCell = activeCell.next();
 
-  activeCell.removeClass("active");
-  moveCell.addClass("active");
+    activeCell.removeClass("active");
+    moveCell.addClass("active");
+
+};
+
+var winnerBox = function (winner) {
+  if (confirm (winner + " wins! Would you like to play again?")){
+    console.log ('yay');
+  };
+
 };
