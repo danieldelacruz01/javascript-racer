ad1ae92f2a2d980ca50730123aa8300a490f2507
Daniel de la Cruz <danieldelacruz01@gmail.com>
2016-01-16 15:01:41 +1300

    change folder directories

---
+++ main.js
@@ -0,0 +1,11 @@
+// var updatePlayerPosition() {
+
+// };
+
+$(document).keydown(function(key){
+  if (key.keyCode === 80) {
+    console.log ("p");
+  } else if (key.keyCode === 81) {
+    console.log ("q");
+  };
+});
