44c930e1460bc70db5bb9f85eaf7fa8d3159b5cd
Daniel de la Cruz <danieldelacruz01@gmail.com>
2016-02-11 21:35:34 +1300

    add obstacles

--- main.js
+++ main.js
@@ -1,93 +1,47 @@
-// $(document).ready(function(){
-//   //start state
-//   var playerMoves = 0;
-//   var blueMoves = 0;
 $(document).ready(function(){
-  var trackLength = 4;
-  
+  var trackLength = 0;
   var moveCount = 0;
 
   var setTrackLength = function(){
-
-    while (trackLength <= 100) {
-      $("#track").prepend(
-          "<tr><td>Lane</td><td>Lane</td><td>Lane</td><td>Lane</td></tr>"
-      );
-      trackLength++
+    for (var i = 0; i <= 100; i++) {
+      if (i > 3 && i < 100) {
+        $("#track").prepend(
+            "<tr><td class='cell one'></td><td class='cell two'></td><td class='cell three'></td><td class='cell four'></td></tr>"
+        );
+        trackLength++
+      } else if (i === 100) {
+        $("#track").prepend(
+            "<tr><td colspan='4'>Finish</td></tr>"
+        );
+      }
     };
   };
 
-  var startGame = function() {
-    
-    $(document).on('keyup', function(key){
+  var createObstacles = function() {
+    $("td.cell").each(function() {
+      var obstacleNumber = Math.floor(Math.random() * 12 + 1);
+      if (obstacleNumber === 1) {
+        $(this).addClass("obstacle");
+      }
+    });
+  };
+
+  var startGame = function() { 
+    $(document).on('keydown', function(key){
       if (key.keyCode === 38) {
-        window.scrollBy(0, -53);
         moveCount++;
+        if (moveCount > 5) {
+          window.scrollBy(0, -50);
+        }
       };
+      if ($("td.active").hasClass("obstacle")) {
+        console.log("ouch");
+      }
     });
     $('table#track > tbody td').keynavigator();
   };
 
   setTrackLength();
+  createObstacles();
   startGame();
-
 });
-
-//   for (var i = 1; i <= trackLength; i++) {
-//       console.log(i);
-//       $(".track").append("<td>");
-//     };
-
-//   var restart = function() {
-//     $("td").removeClass("active");
-//     $("#blue td:first").addClass("active");
-//     $("#red td:first").addClass("active");
-
-//     redMoves = 0;
-//     blueMoves = 0;
-//   };
-
-    
-
-//   restart();
-
-//   // keydown event
-//   $(document).on('keyup', function(key){
-
-//     if (key.keyCode === 38) {
-
-//       if (playerMoves < trackLength - 1){
-//         updatePlayerPosition("red");
-//         redMoves++;
-//       } else {
-//         winnerBox("Red")
-//       };
-
-//     } else if (key.keyCode === 81) {
-
-//       if (blueMoves < trackLength - 1){
-//         updatePlayerPosition("blue");
-//         blueMoves++;
-
-//       } else {
-//         winnerBox("Blue")
-//       };
-//     };
-//   });
-
-//   //move active td
-//   var updatePlayerPosition = function (playerColor) {
-//     var activeCell = $("#" + playerColor + " td.active");
-//     var moveCell = activeCell.next();
-
-//     activeCell.removeClass("active");
-//     moveCell.addClass("active");
-//   };
-
-//   //winning state
-//   var winnerBox = function (winner) {
-//     window.alert (winner + " wins! Press OK to play again.")
-//     restart();
-
-//   };
-// });
