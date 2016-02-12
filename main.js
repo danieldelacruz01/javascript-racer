e3033c3c51d8aa2853d1b4d9704ade5ab5a072ba
Daniel de la Cruz <danieldelacruz01@gmail.com>
2016-02-13 12:39:25 +1300

    add finish line

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
+$(window).load(function() {
+  $("html, body").animate({ scrollTop: $(document).height()}, 1000);
+});
+
 $(document).ready(function(){
   var trackLength = 0;
   var moveCount = 0;
 
   var setTrackLength = function(){
     for (var i = 0; i <= 100; i++) {
-      if (i > 3 && i < 100) {
+      if (i < 100) {
         $("#track").prepend(
             "<tr><td class='cell one'></td><td class='cell two'></td><td class='cell three'></td><td class='cell four'></td></tr>"
         );
         trackLength++
       } else if (i === 100) {
         $("#track").prepend(
-            "<tr><td colspan='4'>Finish</td></tr>"
+            "<tr><td colspan='4'>Finish</td></tr><tr><td class='finish'></td><td class='finish'></td><td class='finish'></td><td class='finish'></td>"
         );
       }
     };
@@ -35,13 +39,15 @@ $(document).ready(function(){
         }
       };
       if ($("td.active").hasClass("obstacle")) {
-        var playAgain = confirm("You crashed and died! Play again?");
+        var playAgain = confirm("You crashed and died!\n\nPlay again?\n");
         if (playAgain == true) {
           location.reload();
         } else if (playAgain == false){
           window.open("http://www.lifehacker.com/top-10-ways-to-become-a-better-driver-1596820145", "_self");
         }
-
+      } else if ($("td.active").hasClass("finish")) {
+        alert("Nice one!\n\nYou finished in " + " seconds\n\nPress OK to play again.")
+        location.reload();
       }
     });
     $('table#track > tbody td').keynavigator();
